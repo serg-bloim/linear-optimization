f62fb4a86cb8f9ddf1503c933d02f591b0fb23b3
sergey <serg.bloim@gmail.com>
2018-05-20 11:48:55 -0400

    ui changes

--- src/karmarkar/ui/MainForm.java
+++ src/karmarkar/ui/MainForm.java
@@ -8,6 +8,7 @@ import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JButton;
 import javax.swing.GroupLayout;
+import javax.swing.InputVerifier;
 import javax.swing.GroupLayout.Alignment;
 import java.awt.Color;
 import javax.swing.LayoutStyle.ComponentPlacement;
@@ -16,13 +17,17 @@ import javax.swing.JTextField;
 import javax.swing.JLabel;
 import javax.swing.JTextPane;
 import javax.swing.SwingConstants;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JCheckBox;
+import javax.swing.JComponent;
 
 public class MainForm extends JFrame {
 
     private JPanel contentPane;
-    private JTextField textField;
-    private JTextField textField_1;
     Configuration config = new Configuration("config.properties");
+    private JTextField txtPrecision;
+    private JTextField txtMaxIters;
 
     /**
      * Launch the application.
@@ -60,69 +65,95 @@ public class MainForm extends JFrame {
         panel.setForeground(Color.BLACK);
         contentPane.add(panel, BorderLayout.WEST);
 
-        JButton btnNewButton = new JButton("Load");
+        JButton btnLoad = new JButton("Load");
+        btnLoad.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent arg0) {
+        		
+        	}
+        });
 
-        JButton btnNewButton_1 = new JButton("Iteration");
-
-        JButton btnNewButton_2 = new JButton("Solve");
-
-        textField = new JTextField();
-        textField.setHorizontalAlignment(SwingConstants.RIGHT);
-        textField.setText(config.getPrecision());
-        textField.setColumns(10);
-
-        textField_1 = new JTextField();
-        textField_1.setHorizontalAlignment(SwingConstants.RIGHT);
-        textField_1.setText(config.getMaxIterations());
-        textField_1.setColumns(10);
-
-        JLabel lblNewLabel = new JLabel("Presicion");
-
-        JLabel lblNewLabel_1 = new JLabel("Max iterations");
+        JButton btnSolve = new JButton("Solve");
+        btnSolve.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent arg0) {
+        	}
+        });
+        
+        JPanel panel_1 = new JPanel();
         GroupLayout gl_panel = new GroupLayout(panel);
         gl_panel.setHorizontalGroup(
-                gl_panel.createParallelGroup(Alignment.LEADING)
-                        .addGroup(gl_panel.createSequentialGroup()
-                                .addContainerGap()
-                                .addGroup(gl_panel.createParallelGroup(Alignment.LEADING)
-                                        .addGroup(Alignment.TRAILING, gl_panel.createSequentialGroup()
-                                                .addComponent(textField, GroupLayout.DEFAULT_SIZE, 108, Short.MAX_VALUE)
-                                                .addGap(20))
-                                        .addGroup(gl_panel.createSequentialGroup()
-                                                .addComponent(textField_1, GroupLayout.DEFAULT_SIZE, 108, Short.MAX_VALUE)
-                                                .addGap(20))
-                                        .addGroup(gl_panel.createSequentialGroup()
-                                                .addComponent(lblNewLabel)
-                                                .addContainerGap(72, Short.MAX_VALUE))
-                                        .addGroup(gl_panel.createSequentialGroup()
-                                                .addComponent(lblNewLabel_1)
-                                                .addContainerGap(72, Short.MAX_VALUE))
-                                        .addGroup(Alignment.TRAILING, gl_panel.createSequentialGroup()
-                                                .addGroup(gl_panel.createParallelGroup(Alignment.TRAILING)
-                                                        .addComponent(btnNewButton, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 132, Short.MAX_VALUE)
-                                                        .addComponent(btnNewButton_2, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 107, Short.MAX_VALUE)
-                                                        .addComponent(btnNewButton_1, GroupLayout.DEFAULT_SIZE, 107, Short.MAX_VALUE))
-                                                .addGap(20))))
+        	gl_panel.createParallelGroup(Alignment.TRAILING)
+        		.addGroup(gl_panel.createSequentialGroup()
+        			.addContainerGap()
+        			.addComponent(btnLoad, GroupLayout.DEFAULT_SIZE, 132, Short.MAX_VALUE)
+        			.addGap(20))
+        		.addComponent(panel_1, GroupLayout.DEFAULT_SIZE, 164, Short.MAX_VALUE)
+        		.addGroup(Alignment.LEADING, gl_panel.createSequentialGroup()
+        			.addContainerGap()
+        			.addComponent(btnSolve, GroupLayout.DEFAULT_SIZE, 132, Short.MAX_VALUE)
+        			.addGap(20))
         );
         gl_panel.setVerticalGroup(
-                gl_panel.createParallelGroup(Alignment.LEADING)
-                        .addGroup(gl_panel.createSequentialGroup()
-                                .addContainerGap()
-                                .addComponent(btnNewButton)
-                                .addPreferredGap(ComponentPlacement.UNRELATED)
-                                .addComponent(btnNewButton_1)
-                                .addPreferredGap(ComponentPlacement.UNRELATED)
-                                .addComponent(btnNewButton_2)
-                                .addGap(21)
-                                .addComponent(lblNewLabel)
-                                .addPreferredGap(ComponentPlacement.RELATED)
-                                .addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-                                .addGap(10)
-                                .addComponent(lblNewLabel_1)
-                                .addPreferredGap(ComponentPlacement.RELATED)
-                                .addComponent(textField_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-                                .addContainerGap(232, Short.MAX_VALUE))
+        	gl_panel.createParallelGroup(Alignment.LEADING)
+        		.addGroup(gl_panel.createSequentialGroup()
+        			.addContainerGap()
+        			.addComponent(btnLoad)
+        			.addGap(18)
+        			.addComponent(btnSolve)
+        			.addGap(51)
+        			.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 251, GroupLayout.PREFERRED_SIZE)
+        			.addContainerGap(84, Short.MAX_VALUE))
         );
+        
+        txtPrecision = new JTextField();
+        txtPrecision.setText("0.0001");
+        txtPrecision.setHorizontalAlignment(SwingConstants.RIGHT);
+        txtPrecision.setColumns(10);
+        
+        txtMaxIters = new JTextField();
+        txtMaxIters.setText("500");
+        txtMaxIters.setHorizontalAlignment(SwingConstants.RIGHT);
+        txtMaxIters.setColumns(10);
+        
+        JLabel lblStopConditions = new JLabel("Stop Conditions :");
+        
+        JCheckBox chkPrecision = new JCheckBox("Precision");
+        
+        JCheckBox chkMaxIters = new JCheckBox("Max iterations");
+        
+        JCheckBox chkOptimumFound = new JCheckBox("Optimum found");
+        GroupLayout gl_panel_1 = new GroupLayout(panel_1);
+        gl_panel_1.setHorizontalGroup(
+        	gl_panel_1.createParallelGroup(Alignment.LEADING)
+        		.addGroup(gl_panel_1.createSequentialGroup()
+        			.addContainerGap()
+        			.addGroup(gl_panel_1.createParallelGroup(Alignment.LEADING)
+        				.addComponent(chkMaxIters)
+        				.addComponent(lblStopConditions, GroupLayout.PREFERRED_SIZE, 140, GroupLayout.PREFERRED_SIZE)
+        				.addComponent(txtPrecision, GroupLayout.PREFERRED_SIZE, 132, GroupLayout.PREFERRED_SIZE)
+        				.addComponent(chkPrecision)
+        				.addGroup(gl_panel_1.createParallelGroup(Alignment.TRAILING, false)
+        					.addComponent(chkOptimumFound, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+        					.addComponent(txtMaxIters, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 132, Short.MAX_VALUE)))
+        			.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+        gl_panel_1.setVerticalGroup(
+        	gl_panel_1.createParallelGroup(Alignment.LEADING)
+        		.addGroup(gl_panel_1.createSequentialGroup()
+        			.addContainerGap()
+        			.addComponent(lblStopConditions)
+        			.addGap(15)
+        			.addComponent(chkPrecision)
+        			.addPreferredGap(ComponentPlacement.RELATED)
+        			.addComponent(txtPrecision, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+        			.addGap(18)
+        			.addComponent(chkMaxIters)
+        			.addPreferredGap(ComponentPlacement.RELATED)
+        			.addComponent(txtMaxIters, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+        			.addGap(18)
+        			.addComponent(chkOptimumFound)
+        			.addContainerGap(46, Short.MAX_VALUE))
+        );
+        panel_1.setLayout(gl_panel_1);
         panel.setLayout(gl_panel);
 
         JTextPane txtpnProblemLoaded = new JTextPane();
